b546c0b1c3aa1f063efa23a8bc4785f74ccc3fd7
Jitu Khamcha <lappanchappan43@gmail.com>
2021-12-31 15:06:57 +0545

    repsequal

---
+++ centered_5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int isCentered5(int a[ ], int len){
+    
+}
+
+int main(){
+    int a[] = {3, 2, 10, 4, 1, 6, 9} ;
+    int len = sizeof(a)/sizeof(a[0]);
+
+    cout << endl;
+    return 0;
+}
