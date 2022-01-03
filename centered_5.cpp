3679094b57fba49c794f05caa4c8cc0eca33d2cb
Jitu Khamcha <lappanchappan43@gmail.com>
2022-01-03 10:25:25 +0545

    Centered 15

--- centered_5.cpp
+++ centered_5.cpp
@@ -2,14 +2,36 @@
 
 using namespace std;
 
-int isCentered5(int a[ ], int len){
+int isCentered15(int a[ ], int len){
+    int lower_index = 0;
+    int upper_index = 0;
+    int sum = 0;
+    if (len%2 == 0){
+        lower_index = (len/2)-1;
+        upper_index = (len/2);
+    }else{
+        lower_index = (len-1)/2;
+        upper_index = lower_index;
+    }
+    while (lower_index >= 0 && upper_index < len){
+        sum = 0;
+        for (int i=lower_index; i<=upper_index; i++){
+            sum += a[i];
+        }
+        if (sum == 15){
+            return 1;
+        }
+        lower_index--;
+        upper_index++;
+    }
     
+    return 0;
 }
 
 int main(){
-    int a[] = {3, 2, 10, 4, 1, 6, 9} ;
+    int a[] = {1, 1, 15 -1,-1};
     int len = sizeof(a)/sizeof(a[0]);
 
-    cout << endl;
+    cout << isCentered15(a, len) << endl;
     return 0;
 }
